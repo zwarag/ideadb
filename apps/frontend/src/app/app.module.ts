5860b4a116dcecd1b1d08a648548185985f25a72
Harrys Kavan <harrys.kavan@pm.me>
2021-07-07 22:04:48 +0200

    create home component

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
